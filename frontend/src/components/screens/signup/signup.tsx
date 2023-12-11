f8654db6fa18ad069e0b6fc5082f8234cb3c8465
Anant Raj <anant.raj@franklinsabers.org>
2023-12-10 20:30:37 -0800

    added button styling

--- frontend/src/components/screens/signup/signup.tsx
+++ frontend/src/components/screens/signup/signup.tsx
@@ -106,80 +106,75 @@ export const SignUp = (): JSX.Element => {
                 />
               </div>
               <div className="absolute w-[388px] top-[154px] left-[31px] font-bold-24px font-[number:var(--bold-24px-font-weight)] text-grayscale-black text-[length:var(--bold-24px-font-size)] text-center tracking-[var(--bold-24px-letter-spacing)] leading-[var(--bold-24px-line-height)] [font-style:var(--bold-24px-font-style)]">
-                Create Account
+                  Create Account
               </div>
               <TextfieldLabel
-                className="!absolute !left-[35px] !w-[388px] !top-[261px]"
-                labelClassName="!tracking-[var(--bold-12px-letter-spacing)] !text-[length:var(--bold-12px-font-size)] ![font-style:var(--bold-12px-font-style)] !font-[number:var(--bold-12px-font-weight)] !font-bold-12px !leading-[var(--bold-12px-line-height)] !w-[388px]"
-                overlapGroupClassName="!w-[388px]"
-                text="Name"
-                text1="NAME"
-                type="text"
-                name="name"
-                register={register("name")}
-                error={errors.name}
-                textfieldClassName="!tracking-[var(--regular-14px-letter-spacing)] !text-[length:var(--regular-14px-font-size)] ![font-style:var(--regular-14px-font-style)] !font-[number:var(--regular-14px-font-weight)] !font-regular-14px !leading-[var(--regular-14px-line-height)] !w-[364px]"
+                  className="!absolute !left-[35px] !w-[388px] !top-[261px]"
+                  labelClassName="!tracking-[var(--bold-12px-letter-spacing)] !text-[length:var(--bold-12px-font-size)] ![font-style:var(--bold-12px-font-style)] !font-[number:var(--bold-12px-font-weight)] !font-bold-12px !leading-[var(--bold-12px-line-height)] !w-[388px]"
+                  overlapGroupClassName="!w-[388px]"
+                  text="Name"
+                  text1="NAME"
+                  type="text"
+                  name = "name"
+                  register={register("name")}
+                  error={errors.name}
+                  textfieldClassName="!tracking-[var(--regular-14px-letter-spacing)] !text-[length:var(--regular-14px-font-size)] ![font-style:var(--regular-14px-font-style)] !font-[number:var(--regular-14px-font-weight)] !font-regular-14px !leading-[var(--regular-14px-line-height)] !w-[364px]"
               />
               <TextfieldLabel
-                className="!absolute !left-[35px] !w-[388px] !top-[342px]"
-                labelClassName="!tracking-[var(--bold-12px-letter-spacing)] !text-[length:var(--bold-12px-font-size)] ![font-style:var(--bold-12px-font-style)] !font-[number:var(--bold-12px-font-weight)] !font-bold-12px !leading-[var(--bold-12px-line-height)] !w-[388px]"
-                overlapGroupClassName="!w-[388px]"
-                text="Email"
-                text1="EMAIL"
-                type="email"
-                register={register("email")}
-                error={errors.email}
-                textfieldClassName="!tracking-[var(--regular-14px-letter-spacing)] !text-[length:var(--regular-14px-font-size)] ![font-style:var(--regular-14px-font-style)] !font-[number:var(--regular-14px-font-weight)] !font-regular-14px !leading-[var(--regular-14px-line-height)] !w-[364px]"
+                  className="!absolute !left-[35px] !w-[388px] !top-[342px]"
+                  labelClassName="!tracking-[var(--bold-12px-letter-spacing)] !text-[length:var(--bold-12px-font-size)] ![font-style:var(--bold-12px-font-style)] !font-[number:var(--bold-12px-font-weight)] !font-bold-12px !leading-[var(--bold-12px-line-height)] !w-[388px]"
+                  overlapGroupClassName="!w-[388px]"
+                  text="Email"
+                  text1="EMAIL"
+                  type="email"
+                  register={register("email")}
+                  error={errors.email}
+                  textfieldClassName="!tracking-[var(--regular-14px-letter-spacing)] !text-[length:var(--regular-14px-font-size)] ![font-style:var(--regular-14px-font-style)] !font-[number:var(--regular-14px-font-weight)] !font-regular-14px !leading-[var(--regular-14px-line-height)] !w-[364px]"
               />
               <TextfieldLabelIcon
-                className="!absolute !left-[35px] !w-[388px] !top-[423px]"
-                iconHideActiveStyleOverrideClassName="!absolute !w-[20px] !h-[20px] !top-[10px] !left-[351px]"
-                labelClassName="!tracking-[var(--bold-12px-letter-spacing)] !text-[length:var(--bold-12px-font-size)] ![font-style:var(--bold-12px-font-style)] !font-[number:var(--bold-12px-font-weight)] !font-bold-12px !leading-[var(--bold-12px-line-height)] !w-[388px]"
-                overlapGroupClassName="!w-[388px]"
-                text="Password"
-                text1="PASSWORD"
-                type="password"
-                register={register("password")}
-                error={errors.password}
-                textfieldClassName="!tracking-[var(--regular-14px-letter-spacing)] !text-[length:var(--regular-14px-font-size)] ![font-style:var(--regular-14px-font-style)] !font-[number:var(--regular-14px-font-weight)] !font-regular-14px !leading-[var(--regular-14px-line-height)] !w-[364px]"
+                  className="!absolute !left-[35px] !w-[388px] !top-[423px]"
+                  iconHideActiveStyleOverrideClassName="!absolute !w-[20px] !h-[20px] !top-[10px] !left-[351px]"
+                  labelClassName="!tracking-[var(--bold-12px-letter-spacing)] !text-[length:var(--bold-12px-font-size)] ![font-style:var(--bold-12px-font-style)] !font-[number:var(--bold-12px-font-weight)] !font-bold-12px !leading-[var(--bold-12px-line-height)] !w-[388px]"
+                  overlapGroupClassName="!w-[388px]"
+                  text="Password"
+                  text1="PASSWORD"
+                  type="password"
+                  register={register("password")}
+                  error={errors.password}
+                  textfieldClassName="!tracking-[var(--regular-14px-letter-spacing)] !text-[length:var(--regular-14px-font-size)] ![font-style:var(--regular-14px-font-style)] !font-[number:var(--regular-14px-font-weight)] !font-regular-14px !leading-[var(--regular-14px-line-height)] !w-[364px]"
               />
               <TextfieldLabel
-                className="!absolute !left-[5px] !w-[388px] !top-[500px]"
-                labelClassName="!tracking-[var(--bold-12px-letter-spacing)] !text-[length:var(--bold-12px-font-size)] ![font-style:var(--bold-12px-font-style)] !font-[number:var(--bold-12px-font-weight)] !font-bold-12px !leading-[var(--bold-12px-line-height)] !w-[388px]"
-                overlapGroupClassName="!w-[388px]"
-                text="Verification Code"
-                text1="VERIFICATION CODE"
-                type="number"
-                register={register("verificationCode")}
-                error={errors.verificationCode}
-                textfieldClassName="!tracking-[var(--regular-14px-letter-spacing)] !text-[length:var(--regular-14px-font-size)] ![font-style:var(--regular-14px-font-style)] !font-[number:var(--regular-14px-font-weight)] !font-regular-14px !leading-[var(--regular-14px-line-height)] !w-[364px]"
+                  className="!absolute !left-[35px] !w-[388px] !top-[500px]"
+                  labelClassName="!tracking-[var(--bold-12px-letter-spacing)] !text-[length:var(--bold-12px-font-size)] ![font-style:var(--bold-12px-font-style)] !font-[number:var(--bold-12px-font-weight)] !font-bold-12px !leading-[var(--bold-12px-line-height)] !w-[388px]"
+                  overlapGroupClassName="!w-[250px]"
+                  text="Verification Code"
+                  text1="VERIFICATION CODE"
+                  type="number"
+                  register={register("verificationCode")}
+                  error={errors.verificationCode}
+                  textfieldClassName="!tracking-[var(--regular-14px-letter-spacing)] !text-[length:var(--regular-14px-font-size)] ![font-style:var(--regular-14px-font-style)] !font-[number:var(--regular-14px-font-weight)] !font-regular-14px !leading-[var(--regular-14px-line-height)] !w-[364px]"
               />
-              <button className="absolute w-[48px] h-[48px] top-[518px] left-[400px] all-[unset] box-border">
-                <div className="relative h-[48px] rounded-[15px]">
-                  <div className="w-[100px] h-[48px] top-0 bg-[#3751ff] rounded-[15px] shadow-button-accent-default absolute left-0" />
-                  <button
-                    onClick={requestVerificationCode}
-                    className="absolute w-[55px] top-[14px] left-[24px] font-semibold-14px font-[number:var(--semibold-14px-font-weight)] text-grayscale-white text-[length:var(--semibold-14px-font-size)] text-center tracking-[var(--semibold-14px-letter-spacing)] leading-[var(--semibold-14px-line-height)] [font-style:var(--semibold-14px-font-style)] all-[unset] box-border"
-                  >
-                    Send{" "}
-                    {verificationCodeCooldownSecondsLeft > 0 &&
-                      ` (${verificationCodeCooldownSecondsLeft})`}
-                  </button>
-                </div>
-              </button>
-              <div className="absolute w-[224px] h-[20px] top-[660px] left-[104px]">
-                <div className="relative w-[222px] h-[20px]">
-                  <div className="absolute top-0 left-0 font-regular-14px font-[number:var(--regular-14px-font-weight)] text-grayscale-gray text-[length:var(--regular-14px-font-size)] text-center tracking-[var(--regular-14px-letter-spacing)] leading-[var(--regular-14px-line-height)] whitespace-nowrap [font-style:var(--regular-14px-font-style)]">
-                    Already have an account?
+              <button className="absolute w-[48px] h-[48px] top-[518px] left-[300px] all-[unset] box-border">
+                  <div className="relative h-[48px] rounded-[15px]">
+                    <div className="w-[100px] h-[48px] top-0 bg-[#3751ff] rounded-[15px] shadow-button-accent-default absolute left-0" />
+                      <button onClick = {requestVerificationCode} className="absolute w-[55px] top-[14px] left-[24px] font-semibold-14px font-[number:var(--semibold-14px-font-weight)] text-grayscale-white text-[length:var(--semibold-14px-font-size)] text-center tracking-[var(--semibold-14px-letter-spacing)] leading-[var(--semibold-14px-line-height)] [font-style:var(--semibold-14px-font-style)] all-[unset] box-border">
+                        Send {verificationCodeCooldownSecondsLeft > 0 && ` (${verificationCodeCooldownSecondsLeft})`}
+                      </button>
                   </div>
-                  <ButtonTextButton
-                    btnLabelClassName="!text-mainblue !tracking-[var(--semibold-14px-letter-spacing)] !text-[length:var(--semibold-14px-font-size)] ![font-style:var(--semibold-14px-font-style)] !font-[number:var(--semibold-14px-font-weight)] !font-semibold-14px !leading-[var(--semibold-14px-line-height)] !w-[51px]"
-                    className="!absolute !left-[171px] !w-[51px] !top-0"
-                    onClick={() => navigate("/login")}
-                    text="Log in"
-                  />
+                </button>
+                <div className="absolute w-[224px] h-[20px] top-[660px] left-[104px]">
+                  <div className="relative w-[222px] h-[20px]">
+                    <div className="absolute top-0 left-0 font-regular-14px font-[number:var(--regular-14px-font-weight)] text-grayscale-gray text-[length:var(--regular-14px-font-size)] text-center tracking-[var(--regular-14px-letter-spacing)] leading-[var(--regular-14px-line-height)] whitespace-nowrap [font-style:var(--regular-14px-font-style)]">
+                      Already have an account?
+                    </div>
+                    <ButtonTextButton
+                      btnLabelClassName="!text-mainblue !tracking-[var(--semibold-14px-letter-spacing)] !text-[length:var(--semibold-14px-font-size)] ![font-style:var(--semibold-14px-font-style)] !font-[number:var(--semibold-14px-font-weight)] !font-semibold-14px !leading-[var(--semibold-14px-line-height)] !w-[51px]"
+                      className="!absolute !left-[171px] !w-[51px] !top-0"
+                      onClick={() => navigate("/login")}
+                      text="Log in"
+                    />
+                  </div>
                 </div>
-              </div>
 
               <button className="absolute w-[388px] h-[48px] top-[595px] left-[31px] all-[unset] box-border">
                 <div className="relative h-[48px] rounded-[8px]">
