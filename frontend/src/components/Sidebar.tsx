bbc89d8c7ae28609aba57bf71dd48243974aa1b9
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2023-12-24 16:36:27 -0600

    Close and open sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,51 +1,68 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import { SiShopware, SiAlwaysdata } from 'react-icons/si'
-import { MdOutlineCancel } from 'react-icons/md'
-import { links } from '../data/dummy';
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware, SiAlwaysdata } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { links } from "../data/dummy";
+
+import { useStateContext } from "../context/ContextProvider";
 
 export const Sidebar = () => {
-    const activeMenu = true;
-    const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white bg-mainblue text-md m-2';
-    const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-mainlightblue m-2';
-    return (
-        <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
-          {activeMenu && (
-            <>
-              <div className="flex justify-between items-center">
-                <Link to="/" onClick={()=> {}} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
-                  <SiShopware /> <span>Power<span className='dark:text-mainlightblue text-mainblue'>Grader</span></span>
-                </Link>
-                  <button
-                    type="button"
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const activeLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white bg-mainblue text-md m-2";
+  const normalLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-mainlightblue m-2";
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link
+              to="/"
+              onClick={() => {}}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
+              <SiShopware />{" "}
+              <span>
+                Power
+                <span className="dark:text-mainlightblue text-mainblue">
+                  Grader
+                </span>
+              </span>
+            </Link>
+            <button
+              type="button"
+              onClick={() => {}}
+              className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+            >
+              <MdOutlineCancel />
+            </button>
+          </div>
+          <div className="mt-10 ">
+            {links.map((item) => (
+              <div key={item.title}>
+                <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
                     onClick={() => {}}
-                    className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
                   >
-                    <MdOutlineCancel />
-                  </button>
-              </div>
-              <div className="mt-10 ">
-                {links.map((item) => (
-                  <div key={item.title}>
-                    <p className="text-gray-400 dark:text-gray-400 m-3 mt-4 uppercase">
-                      {item.title}
-                    </p>
-                    {item.links.map((link) => (
-                      <NavLink
-                        to={`/${link.name}`}
-                        key={link.name}
-                        onClick={()=>{}}
-                        className={({ isActive }) => (isActive ? activeLink : normalLink)}
-                      >
-                        {link.icon}
-                        <span className="capitalize ">{link.name}</span>
-                      </NavLink>
-                    ))}
-                  </div>
+                    {link.icon}
+                    <span className="capitalize ">{link.name}</span>
+                  </NavLink>
                 ))}
               </div>
-            </>
-          )}
-        </div>
-      );
-}
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
