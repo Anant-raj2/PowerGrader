4ac07e0a0823fa9f0c8f3b3fec6449860d8845f3
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2023-12-25 15:32:09 -0600

    EMAIL TEMPLATE

--- backend/src/util/email.ts
+++ backend/src/util/email.ts
@@ -1,20 +1,70 @@
-import {createTransport} from 'nodemailer';
+import { createTransport } from "nodemailer";
 import env from "./validateEnv";
 
 const transporter = createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    auth: {
-        user: "heyanantraj@gmail.com",
-        pass: env.SMTP_PASSWORD
-    }
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  auth: {
+    user: "heyanantraj@gmail.com",
+    pass: env.SMTP_PASSWORD,
+  },
 });
 
-export async function sendVerificationEmail(toEmail: string, verificationCode: string) {
-    await transporter.sendMail({
-        from: "heyanantraj@gmail.com",
-        to: toEmail,
-        subject: "Verify your email",
-        html: `<h1>Verify your email</h1><br><p>Enter this code to verify your email: <strong>${verificationCode}</strong></p>`
-    })
+export async function sendVerificationEmail(
+  toEmail: string,
+  verificationCode: string
+) {
+  await transporter.sendMail({
+    from: "noreply@powergrader.com",
+    to: toEmail,
+    subject: "Verify your email",
+    html: `<!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #FFFFFF;
+                color: #1E1E1E;
+            }
+            .container {
+                width: 100%;
+                margin: 0 auto;
+                background-color: #FFFFFF;
+            }
+            .header {
+                background-color: #3751FF;
+                color: #FFFFFF;
+                padding: 20px;
+                text-align: center;
+            }
+            .content {
+                padding: 20px;
+                text-align: center;
+            }
+            .verification-code {
+                font-size: 20px;
+                font-weight: bold;
+                color: #3751FF;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="header">
+                <h1>PowerGrader</h1>
+            </div>
+            <div class="content">
+                <p>Hello,</p>
+                <p>Please verify your email address by entering the code below:</p>
+                <p class="verification-code">${verificationCode}</p>
+                <p>Thank you for using PowerGrader!</p>
+            </div>
+        </div>
+    </body>
+    </html>
+    `,
+  });
 }
