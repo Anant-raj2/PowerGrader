69998dda5ff46974ca15e3ba53118703a13f08c6
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2023-12-28 13:27:33 -0600

    Got rid of useless tings

--- frontend/src/components/UserProfile.tsx
+++ frontend/src/components/UserProfile.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-
-import { userProfileData } from "../data/dummy";
-import { MdOutlineCancel } from "react-icons/md";
 import Button from "./Button";
+import { MdOutlineCancel } from "react-icons/md";
 import * as User from "../networks/api/user_api";
 import { useNavigate } from "react-router-dom";
 
 export const UserProfile = (): JSX.Element => {
   const navigate = useNavigate();
+
   const logout = () => {
     User.logout();
     navigate("/login");
@@ -29,31 +28,6 @@ export const UserProfile = (): JSX.Element => {
           width={""}
           onClick={() => {}}
         />
-      </div>
-
-      <div>
-        {userProfileData.map((item, index) => (
-          <div
-            key={index}
-            className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]"
-          >
-            <button
-              type="button"
-              style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-              className=" text-xl rounded-lg p-3 hover:bg-light-gray"
-            >
-              {item.icon}
-            </button>
-
-            <div>
-              <p className="font-semibold dark:text-gray-200 ">{item.title}</p>
-              <p className="text-gray-500 text-sm dark:text-gray-400">
-                {" "}
-                {item.desc}{" "}
-              </p>
-            </div>
-          </div>
-        ))}
       </div>
 
       <div className="mt-5">
