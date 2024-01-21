3c0bb7790a9c3950ee424eff5be8119c0cd10958
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-01-20 20:16:57 -0600

    NEWCODDE

--- frontend/src/screens/overview.tsx
+++ frontend/src/screens/overview.tsx
@@ -8,19 +8,24 @@ export const Overview = (): JSX.Element => {
       <div className="mt-14">
         <div className="flex flex-wrap ml-22">
           <div className="flex flex-row">
-            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-96 p-8 pt-9 m-5 bg-hero-pattern bg-no-repeat bg-cover bg-center">
-              <div>
-                <p className="font-bold text-gray-400">GPA</p>
+            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-96 p-8 pt-9 m-5 bg-hero-pattern bg-no-repeat bg-cover bg-center flex items-start justify-center">
+              <div className="flex flex-col items-center">
+                <p className="font-normal text-gray-400 text-2xl">GPA</p>
+                <p className="font-bold text-gray-400 text-5xl pt-3">4.0</p>
               </div>
             </div>
-            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-96 p-8 pt-9 m-5 bg-hero-pattern bg-no-repeat bg-cover bg-center">
-              <div>
-                <p className="font-bold text-gray-400">Weighted GPA</p>
+            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-96 p-8 pt-9 m-5 bg-hero-pattern bg-no-repeat bg-cover bg-center flex items-start justify-center">
+              <div className="flex flex-col items-center">
+                <p className="font-normal text-gray-400 text-2xl">
+                  Weighted GPA
+                </p>
+                <p className="font-bold text-gray-400 text-5xl pt-3">4.20</p>
               </div>
             </div>
-            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-96 p-8 pt-9 m-5 bg-hero-pattern bg-no-repeat bg-cover bg-center">
-              <div>
-                <p className="font-bold text-gray-400">Rating</p>
+            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-96 p-8 pt-9 m-5 bg-hero-pattern bg-no-repeat bg-cover bg-center flex items-start justify-center">
+              <div className="flex flex-col items-center">
+                <p className="font-normal text-gray-400 text-2xl">Rating</p>
+                <p className="font-bold text-gray-400 text-5xl pt-3">Average</p>
               </div>
             </div>
           </div>
@@ -28,18 +33,20 @@ export const Overview = (): JSX.Element => {
         <div className="flex flex-row justify-start sm:ml-13 md:ml-13 lg:ml-20">
           <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ml-13 h-128 rounded-xl w-810 lg:w-150 p-8 pt-9 m-5 bg-hero-pattern bg-no-repeat bg-cover bg-center">
             <div>
-              <p className="font-bold text-gray-400">Your Overview</p>
+              <p className="font-bold text-gray-400 text-2xl">Your Overview</p>
             </div>
           </div>
           <div className="flex flex-col">
-            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ml-4 h-59 rounded-xl w-full lg:w-96 p-8 pt-9 m-5 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ml-4 h-59 rounded-xl w-full lg:w-96 p-8 pt-9 m-5 bg-hero-pattern bg-no-repeat bg-cover bg-center flex items-start justify-center">
               <div>
-                <p className="font-bold text-gray-400">Best Classes</p>
+                <p className="font-bold text-gray-400 text-2xl">Best Classes</p>
               </div>
             </div>
-            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ml-4 h-59 rounded-xl w-full lg:w-96 p-8 pt-9 m-5 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg ml-4 h-59 rounded-xl w-full lg:w-96 p-8 pt-9 m-5 bg-hero-pattern bg-no-repeat bg-cover bg-center flex items-start justify-center">
               <div>
-                <p className="font-bold text-gray-400">Worst Classes</p>
+                <p className="font-bold text-gray-400 text-2xl">
+                  Worst Classes
+                </p>
               </div>
             </div>
           </div>
