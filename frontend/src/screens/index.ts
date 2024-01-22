ea3894ca20cba84f951497c3a7630901db931d26
Anant-raj2 <heyanantraj@gmail.com>
2024-01-21 18:30:58 -0600

    final

--- frontend/src/screens/index.ts
+++ frontend/src/screens/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export { Login } from "./auth/login";
 export { SignUp } from "./auth/signup";
 export { Onboarding } from "./auth/onboarding";
@@ -10,18 +9,3 @@ export { Transcript } from "./transcript";
 export { NotAuthNav } from "../layouts/notAuthNav";
 export { Booster } from "./booster";
 export { Class } from "./class";
-=======
-export { Login } from './auth/login';
-export { SignUp } from './auth/signup';
-export { Onboarding } from './auth/onboarding';
-export { StudentDashboard } from '../layouts/studentDashboard';
-export { PageNotFound } from './pageNotFound';
-export { Overview } from './overview';
-export { Contact } from './contact';
-export { Line } from './line';
-export { Transcript } from './transcript';
-export { NotAuthNav } from '../layouts/notAuthNav';
-export { Booster } from './booster';
-export { Class } from './class'
-export { Upload } from './upload';
->>>>>>> 3f5f7b9dfab95355f7c34ba89c56fd2d35d8f46d
