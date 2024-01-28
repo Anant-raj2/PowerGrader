708ab9afd54f5bc35382832dc3255827a5a9d456
Kanwar-Ghuman <118076208+Kanwar-Ghuman@users.noreply.github.com>
2024-01-27 22:51:30 -0600

    Started Work on da Transcript page

--- frontend/src/screens/booster.tsx
+++ frontend/src/screens/booster.tsx
@@ -1,8 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const Booster = () => {
-  return (
-    <h1 className='text-5xl dark: text-white'>
-      Booster
-    </h1>   )
-}
+  return <h1 className="text-5xl dark: text-white">Coming Soon</h1>;
+};
--- frontend/src/screens/transcript.tsx
+++ frontend/src/screens/transcript.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { Box, Table, Thead, Tbody, Tr, Th, Td, Text } from "@chakra-ui/react";
+import useAuthenticatedUser from "../hooks/useAuthenticatedUser";
 
-export const Transcript = () => {
-  return (
-    <h1 className='text-5xl dark: text-white'>
-      Transcript
-    </h1>   
-    )
+interface ClassItem {
+  class: string;
+  credit: number;
+  grade: string;
+  _id: string;
 }
+
+interface User {
+  // ... other properties of User
+  classes: ClassItem[];
+}
+
+export const Transcript: React.FC = () => {
+  const classes = Array(8).fill({ name: "", grade: "", credits: 0 });
+  const { user } = useAuthenticatedUser() as { user: User };
+  return (
+    <Box className="bg-[#363740] p-4 text-white">
+      <Text className="text-2xl mb-4 text-center text-white">Transcript</Text>
+      <Table variant="simple" size="sm" colorScheme="blue">
+        <Thead>
+          <Tr>
+            <Th>Class</Th>
+            <Th>Grade</Th>
+            <Th isNumeric>Credits</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {user! &&
+            user!.classes.map((classInfo) => (
+              <Tr key={classInfo._id}>
+                <Td>{classInfo.class}</Td>
+                <Td>{classInfo.grade}</Td>
+                <Td isNumeric>{classInfo.credit}</Td>
+              </Tr>
+            ))}
+        </Tbody>
+      </Table>
+    </Box>
+  );
+};
